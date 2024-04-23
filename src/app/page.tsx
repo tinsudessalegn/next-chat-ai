84b73970f97b2f14a9804688dadd9657794a342a
unknown <tinsae.yirgu@ethernet.edu.et>
2024-04-23 20:36:21 +0300

    preparing the chat component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-
+import Chat from "./components/chat"
 
 export const dynamic = 'force-dynamic'
 
 
 export default function Page() {
-  return "next ui chat"
+  return (
+    <div>
+      <Chat/>
+    </div>
+  )
 }
